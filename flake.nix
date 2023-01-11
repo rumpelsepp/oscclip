bf546eb92e21db48c0bf149f6628e32542d6e599
Stefan Tatschner <stefan@rumpelsepp.org>
2023-01-11 10:52:24 +0100

    Update stuff, drop slow linters

---
+++ flake.nix
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: AISEC Pentesting Team
+#
+# SPDX-License-Identifier: CC0-1.0
+
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    with import nixpkgs { system = "x86_64-linux"; };
+    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        buildInputs = with pkgs; [ 
+          poetry
+          python310
+        ];
+        shellHook = ''
+          LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [stdenv.cc.cc]}
+        '';
+      };
+      formatter.x86_64-linux = pkgs.nixpkgs-fmt;
+    };
+}
